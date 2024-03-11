b80688fd1a64e777bcf17f8ccf78fc3d09274c97
iSmo0ke <jesus.alt.2004@gmail.com>
2024-03-11 03:13:42 -0600

    Primer actualizacion

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ROUTE FINDER</title>
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="./img/ico.jpg" alt="Logo">
+        </div>
+
+        <div class="user-actions">
+            <a href="../views/login.php">Iniciar sesión</a>
+            <a href="../views/registro.php">Registrarse</a>
+        </div>
+    </header>
+</body>

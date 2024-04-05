9c8f2f774f13ca8878d5460259f5d8e362b7d2ff
iSmo0ke <jesus.alt.2004@gmail.com>
2024-04-05 00:40:25 -0600

    Muestreo de comentarios

--- controllers/comentarios.js
+++ controllers/comentarios.js
@@ -1,13 +1,54 @@
-function guardarComentario(){
+//Inicializar firestore
+var firebaseConfig = {
+    apiKey: "AIzaSyDRplYQcLl5p6G1fW4IBAwbWgYKMnri9eM",
+    authDomain: "route-finder-75a0b.firebaseapp.com",
+    projectId: "route-finder-75a0b",
+    storageBucket: "route-finder-75a0b.appspot.com",
+    messagingSenderId: "833216771763",
+    appId: "1:833216771763:web:cbaec2a9392817195222c6",
+    measurementId: "G-HMMHJH8PPC"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+//Funcion para registrar comentarios
+function registrarComentario(event) {
+    event.preventDefault();
+
+    var email = document.getElementById("email").value;
+    var name = document.getElementById("name").value;
+    var comment = document.getElementById("comment").value;
+
     db.collection("Comentarios").add({
-        nombre: document.getElementById ("name").value,
-        correo: document.getElementById ("email").value,
-        comentario: document.getElementById ("comment").value
+        email: email,
+        name: name,
+        comment: comment
     })
-    .then((docRef) => {
-        alert ("Comentario enviado con éxito");
-    })
-    .catch((error) => {
-        alert ("Error al enviar comentario");
-    });
+        .then(function (docRef) {
+            console.log("Comentario registrado con ID: ", docRef.id);
+            alert("Comentario registrado con éxito.");
+            location.reload();
+        })
+        .catch(function (error) {
+            console.error("Error al registrar el comentario: ", error);
+            alert("Error al registrar el comentario. Por favor, inténtalo de nuevo.");
+        });
 }
+
+//Funcion para mostrar comentarios
+var tabla = document.getElementById('tabla');
+db.collection("Comentarios").get().then((querySnapshot) => {
+    tabla.innerHTML= '';
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+        tabla.innerHTML += `
+        <tr>
+            <th scope="row">${doc.id}</th>
+            <td>${doc.data().name}</td>
+            <td>${doc.data().email}</td>
+            <td>${doc.data().comment}</td>
+        </tr>
+        `
+    });
+});
+
--- public/views/comentarios.php
+++ public/views/comentarios.php
@@ -2,75 +2,58 @@
     <?php
     include '../includes/header.php';
     ?>
-    <link rel="Icon" type="png" href='/RF-PHP/public/assets/img/icono.jpg'>
-    <script src="https://www.gstatic.com/firebasejs/8.9.0/firebase.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-auth.js"></script>
-    <script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-firestore.js"></script>
-    <script src="../../controllers/comentarios.js"></script>
-    <<script>
-        var firebaseConfig = {
-        apiKey: "AIzaSyDRplYQcLl5p6G1fW4IBAwbWgYKMnri9eM",
-        authDomain: "route-finder-75a0b.firebaseapp.com",
-        projectId: "route-finder-75a0b",
-        storageBucket: "route-finder-75a0b.appspot.com",
-        messagingSenderId: "833216771763",
-        appId: "1:833216771763:web:cbaec2a9392817195222c6",
-        measurementId: "G-HMMHJH8PPC"
-        };
-        firebase.initializeApp(firebaseConfig);
-        const db = firebase.firestore();
-
-        function registrarComentario(event) {
-        event.preventDefault();
-
-        var email = document.getElementById("email").value;
-        var name = document.getElementById("name").value;
-        var comment = document.getElementById("comment").value;
-
-        
-        db.collection("Comentarios").add({
-        email: email,
-        name: name,
-        comment: comment
-        })
-        .then(function(docRef) {
-        console.log("Comentario registrado con ID: ", docRef.id);
-        alert("Comentario registrado con éxito.");
-        })
-        .catch(function(error) {
-        console.error("Error al registrar el comentario: ", error);
-        alert("Error al registrar el comentario. Por favor, inténtalo de nuevo.");
-        });
-        }
-        </script>
-
 </header>
-<!-- comment_form.php -->
 
-<div class="m-5">
-    <form onsubmit="registrarComentario(event)" class="col-md-4" id="comentarioForm">
-        <div class="form-group">
-            <label for="name">Nombre:</label>
-            <input type="text" class="form-control" id="name" name="name" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Correo electrónico:</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="comment">Comentario:</label>
-            <textarea class="form-control" id="comment" name="comment" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary" onclick="guardar()">Enviar comentario</button>
-    </form>
+<div class="row">
+    <!-- Columna del formulario -->
+    <div class="col-md-6">
+        <h2>Deja un comentario</h2>
+        <form onsubmit="registrarComentario(event)" id="comentarioForm">
+            <div class="form-group">
+                <label for="name">Nombre:</label>
+                <input type="text" class="form-control" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Correo electrónico:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="comment">Comentario:</label>
+                <textarea class="form-control" id="comment" name="comment" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar comentario</button>
+        </form>
+    </div>
+    <!-- Columna de los comentarios -->
+    <div class="col-md-6">
+        <h2>Comentarios</h2>
+        <div id="comentarios-container"></div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Comentario</th>
+                </tr>
+            </thead>
+            <tbody id="tabla">
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <footer>
-
     <?php
     include '../includes/footer.php';
     ?>
 </footer>
 
-<script type="module" src="../../controllers/firebaseconect.js"></script>
+
+<link rel="Icon" type="png" href='/RF-PHP/public/assets/img/icono.jpg'>
+<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-firestore.js"></script>
+<script src="../../controllers/comentarios.js"></script>
+

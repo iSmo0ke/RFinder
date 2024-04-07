fa20ce0aaf8291e0490c08548cf33233e21c8fda
iSmo0ke <jesus.alt.2004@gmail.com>
2024-04-07 01:24:42 -0600

    Mostrar los 3 mapas

--- public/views/mapa.php
+++ public/views/mapa.php
@@ -1,68 +1,70 @@
 <?php 
     include '../includes/header.php';
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Route Finder</title>
-    <link rel="stylesheet" href="../css/maps2.css">
-    <link rel="stylesheet" href="../css/fonts.css">
     <link rel="Icon" type="png" href='img/ico.jpg'>
-    <script src="Busqueda.php"></script>
+    <!-- Agregar Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .map-container {
+            height: 70vh; /* Cambiar altura del mapa para que ocupe el 70% de la altura de la pantalla */
+        }
+    </style>
 </head> 
-<body style=" background-color: #023246;">
-    
-    <div>
-        <header class="header">
-            <div class="container">
-                <div class="btn-menu">
-                    <label for="btn-menu" class="icon-menu"></label>
-                </div>
-                <div class="logo">
-                    <h1>Route Finder</h1>
-                </div>
-                <nav class="menu">
-                    <a href="#"></a>
-                    <a href="#"></a>
-                    <a href="#"></a>
-                </nav>
+<body>
+    <div class="container-fluid m-5">
+        <!-- Botones para cambiar el mapa -->
+        <div class="row mb-3">
+            <div class="col">
+                <button class="btn btn-primary btn-block" onclick="cambiarMapa('mapa1')">Mapa 1</button>
             </div>
-        </header>
-        <input type="checkbox" id="btn-menu">
-        <div class="container-menu">
-            <div class="cont-menu">
-                <nav>
-                    <a href="#" <span class="icon-user">Mi perfil</span> </a>
-                    <a href="./index.html" <span class="icon-home">Inicio</span> </a>
-                    <a href="#" <span class="icon-cog">Configuración</span> </a>
-                    <a href="#" <span class="icon-info-circled">Nosotros</span> </a>
-                    <a href="#" <span class="icon-help-circled">Ayuda</span> </a>
-                    <a href="#" <span class="icon-instagram">Redes Sociales</span> </a>
-                </nav>
-                <label for="btn-menu" class="icon-cancel"></label>
-            </div>   
+            <div class="col">
+                <button class="btn btn-primary btn-block" onclick="cambiarMapa('mapa2')">Mapa 2</button>
+            </div>
+            <div class="col">
+                <button class="btn btn-primary btn-block" onclick="cambiarMapa('mapa3')">Mapa 3</button>
+            </div>
+        </div>
+        <!-- Contenedor del mapa -->
+        <divc class="col-md-6">
+            <iframe id="mapa1" src='https://www.google.com/maps/d/embed?mid=1aRY6IAXcA3quC_3DkJPZ6hwJI3xVMtk&ehbc=2E312F' width="100%" height="500px"></iframe>
         </div>
     </div>
 
-    <div class="map-container">
-        <iframe src="https://www.google.com/maps/d/u/2/embed?mid=11SJ35p3-fvnGVlYEPxuxgz2XjPGwff4&ehbc=2E312F" width="100%" height="250%"></iframe>
-    </div>
+    <!-- Agregar Bootstrap JS y jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+    <script>
+        // Función para cambiar el mapa en el iframe
+        function cambiarMapa(mapaId) {
+            var iframe = document.getElementById('mapa1');
+            // Cambiar la URL del mapa en el iframe según el mapa seleccionado
+            switch (mapaId) {
+                case 'mapa1':
+                    iframe.src = 'https://www.google.com/maps/d/embed?mid=1aRY6IAXcA3quC_3DkJPZ6hwJI3xVMtk&ehbc=2E312F';
+                    break;
+                case 'mapa2':
+                    iframe.src = 'https://www.google.com/maps/d/embed?mid=11SJ35p3-fvnGVlYEPxuxgz2XjPGwff4&ehbc=2E312F';
+                    break;
+                case 'mapa3':
+                    iframe.src = 'https://www.google.com/maps/d/embed?mid=1DLOu9GDx2s9A-LxH0Xi5s0v_7A89xVg&ehbc=2E312F';
+                    break;
+                default:
+                    break;
+            }
+        }
+    </script>
 
-
-
-
-    <div id="resultados">
-
-    </div>
+    <?php
+    include '../includes/footer.php';
+    ?>
 </body>
 </html>
-
-<?php
-include '../includes/footer.php';
-?>
